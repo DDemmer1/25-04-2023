7f1cccd67237c21ff2e8277f627db1b00eaaf7ff
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-04-25 11:08:27 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,6 +1,45 @@
 package de.demmer.dennis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+	
+	
+	//main + STRG + LEERTASTE -> ENTER
+	public static void main(String[] args) {
+		
+		List<Integer> liste = new ArrayList<Integer>();
+		
+		liste.add(5);
+		liste.add(9);
+		liste.add(1);
+
+
+		
+		for(int i = 0;i < liste.size();i++) {
+			
+//			int currentInt = liste.get(i);
+//			System.out.println(currentInt);
+			
+			System.out.println(liste.get(i));
+			
+		}
+		
+		
+		liste.remove(0);
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
---
+++ src/de/demmer/dennis/Main.java
@@ -0,0 +1,40 @@
+package de.demmer.dennis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int[] intArray = new int[3];
+		intArray[0] = 1;
+		intArray[1] = 5;
+		intArray[2] = 7;
+		
+//		System.out.println(intArray[1]);
+//		System.out.println(intArray.length);
+
+		
+		
+		//STRG + SHIFT + O
+		List<Integer> liste1 = new ArrayList<Integer>();
+		liste1.add(1);
+		liste1.add(5);
+		liste1.add(7);
+		
+		
+//		System.out.println(liste1.get(1));
+//		System.out.println(liste1.contains(5));
+//		System.out.println(liste1.remove(2));
+//		System.out.println(liste1.isEmpty());
+//		System.out.println(liste1.size());
+		
+
+		
+		
+		
+	}
+	
+
+}
